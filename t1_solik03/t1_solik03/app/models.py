acc456e508c422f5eedfb9b011958b581a04fc92
Aleksandr <solik1995@gmail.com>
2017-03-05 12:17:01 +0200

    add models

--- t1_solik03/t1_solik03/app/models.py
+++ t1_solik03/t1_solik03/app/models.py
@@ -4,6 +4,17 @@ Definition of models.
 
 from django.db import models
 
-
+# Model about me
+class Me(models.Model):
+	Name = models.CharField(max_length = 30)
+	Last_name = models.CharField(max_length = 30)
+	Date_of_birth = models.DateField()
+	# Contacts
+	Email = models.EmailField(max_length=30,blank=True)
+	Jabber = models.EmailField(max_length=30,blank=True)
+	Skype = models.CharField(max_length = 30)
+	# Other contacts
+	Other_contacts = models.TextField(validators=[MaxLengthValidator(200)])
+	Bio = models.TextField(validators=[MaxLengthValidator(200)])
 	
 
